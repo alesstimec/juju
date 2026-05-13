b8fc81fc3d0a3bf69a59d9a4c42f139385aa78cc
Ales Stimec <ales.stimec@canonical.com>
2026-05-13 10:44:35 +0200

    feat(uniter): gate startup on iaas container runner

--- internal/worker/deployer/unit_manifolds.go
+++ internal/worker/deployer/unit_manifolds.go
@@ -248,6 +248,7 @@ func UnitManifolds(config UnitManifoldsConfig) dependency.Manifolds {
 			APICallerName:         apiCallerName,
 			S3CallerName:          s3CallerName,
 			TraceName:             traceName,
+			ContainerRunnerName:   iaasContainerRunnerName,
 			MachineLock:           config.MachineLock,
 			Clock:                 config.Clock,
 			LeadershipTrackerName: leadershipTrackerName,
--- internal/worker/deployer/unit_manifolds_test.go
+++ internal/worker/deployer/unit_manifolds_test.go
@@ -197,6 +197,7 @@ var expectedUnitManifoldsWithDependencies = map[string][]string{
 		"api-config-watcher",
 		"charm-dir",
 		"hook-retry-strategy",
+		"iaas-container-runner",
 		"leadership-tracker",
 		"migration-fortress",
 		"migration-inactive-flag",
